786fc2bcfeb5b3b099b8cd294148f0029a450d40
Samet Atalar <sametyusa@gmail.com>
2019-11-01 09:01:47 +0300

    Update index.php
    security fix and deleted comment lines

--- index.php
+++ index.php
@@ -165,38 +165,6 @@ if (isset($_SESSION["sessionid"])) {
               </a>
             </div>
           </div>
-          <!--
-          <div class="col-xl-3 col-sm-6 mb-3">
-            <div class="card text-white bg-success o-hidden h-100">
-              <div class="card-body">
-                <div class="card-body-icon">
-                  <i class="fas fa-fw fa-shopping-cart"></i>
-                </div>
-                <div class="mr-5">123 New Orders!</div>
-              </div>
-              <a class="card-footer text-white clearfix small z-1" href="#">
-                <span class="float-left">View Details</span>
-                <span class="float-right">
-                  <i class="fas fa-angle-right"></i>
-                </span>
-              </a>
-            </div>
-          </div>
-          <div class="col-xl-3 col-sm-6 mb-3">
-            <div class="card text-white bg-danger o-hidden h-100">
-              <div class="card-body">
-                <div class="card-body-icon">
-                  <i class="fas fa-fw fa-life-ring"></i>
-                </div>
-                <div class="mr-5">13 New Tickets!</div>
-              </div>
-              <a class="card-footer text-white clearfix small z-1" href="#">
-                <span class="float-left">View Details</span>
-                <span class="float-right">
-                  <i class="fas fa-angle-right"></i>
-                </span>
-              </a>
-            </div>-->
           </div>
         </div>
 
@@ -224,26 +192,13 @@ if (isset($_SESSION["sessionid"])) {
                 <th>Klasör</th>
                 <th>Dosya</th>
                 <th>Uygulama</th>
-                <th>İntertech Client Erişimi</th>
-                <th>Deniz Prod Erişimi</th>
+                <th>Client Erişimi</th>
+                <th>Prod Erişimi</th>
                 <th>İnternetten Erişim</th>
                 <th>Kapsam</th>
             </tr>
         </thead>
-        <!--<tfoot>
-            <tr>
-            <th>Tam URL</th>
-                <th>Sunucu</th>
-                <th>Sunucu IP</th>
-                <th>Klasör</th>
-                <th>Dosya</th>
-                <th>Uygulama</th>
-                <th>İntertech Client Erişimi</th>
-                <th>Deniz Prod Erişimi</th>
-                <th>İnternetten Erişim</th>
-                <th>Kapsam</th>
-            </tr>
-        </tfoot>-->
+ 
     </table>
     </div>
     </div>
@@ -348,7 +303,7 @@ if (isset($_SESSION["sessionid"])) {
                           $_SESSION["username"]=$uname;
                           header("Location: index.php");
                         } else {
-                          echo "<br><p class='error'>Kullanıcı adı ya da parola yanlış</p>";
+                             echo "<p><br /><span class='error'>Erişim yetkiniz yok.</span><br /><br /><img src='../no-entry.jpg' width=300 height=200 alt='Access Denied'></p>";
                         }
                       }
                       else echo "<p><br /><span class='error'>Erişim yetkiniz yok.</span><br /><br /><img src='../no-entry.jpg' width=300 height=200 alt='Access Denied'></p>";
